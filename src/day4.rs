dfa94c3c37394448bf1474591aa41c6c2d001992
Nick Chadwick <chadnickbok@gmail.com>
2022-12-04 16:35:15 -0800

    day4 star1

---
+++ src/day4.rs
@@ -0,0 +1,45 @@
+use crate::utils;
+use std::str::FromStr;
+use std::error::Error as StdError;
+
+struct Range {
+    l: usize,
+    r: usize,
+}
+
+impl Range {
+    fn new(raw_range: &str) -> Result<Range, utils::AocError> {
+        let i = raw_range.find("-").expect("Invalid range");
+
+        match (usize::from_str(&raw_range[..i]), usize::from_str(&raw_range[i + 1..])) {
+            (Ok(l), Ok(r)) => return Ok(Range{l, r}),
+            _ => Err(utils::AocError::new("bad string")),
+        }
+    }
+
+    fn contains(&self, r2: &Range) -> bool {
+        if self.l <= r2.l && self.r >= r2.r {
+            return true;
+        }
+        false
+    }
+}
+
+
+pub fn star1(filename: &str) -> Result<usize, utils::AocError> {
+    let lines = utils::read_lines(filename).expect("failed to read lines from file");
+    let mut total = 0;
+
+    for raw_line in lines {
+        let line = raw_line.expect("failed to red line");
+        let i = line.find(",").expect("invalid range pair");
+        let r1 = Range::new(&line[..i]).expect("bad range 1");
+        let r2 = Range::new(&line[i+1..]).expect("bad range 2");
+
+        if r1.contains(&r2) || r2.contains(&r1) {
+            total += 1;
+        }
+    }
+
+    Ok(total)
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,3 +7,20 @@ pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> 
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct AocError {
+    err: String
+}
+
+impl AocError {
+    pub fn from<T: std::error::Error>(t: T) -> AocError {
+        AocError{ err: format!("{}", t) }
+    }
+
+    pub fn new<T: Into<String>>(t: T) -> AocError {
+        AocError{ err: t.into() }
+    }
+
+
+}
