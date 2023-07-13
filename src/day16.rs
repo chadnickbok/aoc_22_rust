410c5743a669fa931df1b9f35ad78a9e0f7e7afd
Nick Chadwick <chadnickbok@gmail.com>
2023-07-13 11:40:23 -0700

    wip more caching

--- src/day16.rs
+++ src/day16.rs
@@ -2,6 +2,7 @@ use crate::utils;
 use anyhow::Result;
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::str::FromStr;
 
@@ -16,6 +17,7 @@ struct Node {
 pub struct ShortestPathCache<'a> {
     nodes: &'a HashMap<String, Node>,
     cache: RefCell<HashMap<(String, String), i64>>,
+    scores: RefCell<HashMap<Vec<&'a str>, (Option<i64>, i64)>>,
 }
 
 impl<'a> ShortestPathCache<'a> {
@@ -23,6 +25,7 @@ impl<'a> ShortestPathCache<'a> {
         ShortestPathCache {
             nodes,
             cache: RefCell::new(HashMap::new()),
+            scores: RefCell::new(HashMap::new()),
         }
     }
 
@@ -55,11 +58,11 @@ impl<'a> ShortestPathCache<'a> {
     fn calculate_shortest_path(&self, start: &str, end: &str) -> i64 {
         let mut visited = HashMap::new();
         for node in self.nodes.keys() {
-            visited.insert(node.clone(), false);
+            visited.insert(node.as_str(), false);
         }
 
         let mut queue = VecDeque::new();
-        queue.push_back((start.to_string(), 0));
+        queue.push_back((start, 0));
 
         while let Some((node, dist)) = queue.pop_front() {
             if node == end {
@@ -68,30 +71,42 @@ impl<'a> ShortestPathCache<'a> {
             if visited[&node] {
                 continue;
             }
-            visited.insert(node.clone(), true);
-            for adjacent in &self.nodes[&node].connections {
-                queue.push_back((adjacent.clone(), dist + 1));
+            visited.insert(&node, true);
+            for adjacent in &self.nodes[node].connections {
+                queue.push_back((adjacent.as_str(), dist + 1));
             }
         }
 
-        0
+        0 // Default to dummy value
     }
-}
 
-fn total_path(
-    nodes: &HashMap<String, Node>,
-    cache: &mut ShortestPathCache,
-    path: &Vec<&str>,
-) -> (Option<i64>, i64) {
-    let mut total_length = 0;
-    let mut total_value = 0;
-    for window in path.windows(2) {
-        let length = cache.shortest_path(&window[0], &window[1]);
-        let cur_node = &nodes[window[1]];
-        total_length += length + 1;
-        total_value += (MAX_I - total_length) * cur_node.value;
+    fn total_path(&self, path: &Vec<&'a str>) -> (Option<i64>, i64) {
+        let val = {
+            match self.scores.borrow().get(path) {
+                Some(x) => *x,
+                None => (None, 0),
+            }
+        };
+        if val.1 > 0 {
+            return val;
+        }
+
+        let mut total_length = 0;
+        let mut total_value = 0;
+        for window in path.windows(2) {
+            let length = self.shortest_path(&window[0], &window[1]);
+            let cur_node = &self.nodes[window[1]];
+            total_length += length + 1;
+            total_value += (MAX_I - total_length) * cur_node.value;
+        }
+
+        let key = path.clone();
+        self.scores
+            .borrow_mut()
+            .insert(key, (Some(total_length), total_value));
+
+        (Some(total_length), total_value)
     }
-    (Some(total_length), total_value)
 }
 
 fn dfs<'a>(
@@ -103,8 +118,8 @@ fn dfs<'a>(
     best_value: &mut i64,
     best_paths: &mut (Vec<&'a str>, Vec<&'a str>),
 ) {
-    let (length1, value1) = { total_path(nodes, cache, path1) };
-    let (length2, value2) = { total_path(nodes, cache, path2) };
+    let (length1, value1) = { cache.total_path(path1) };
+    let (length2, value2) = { cache.total_path(path2) };
 
     // If either path is too long, stop exploring this branch
     if let (Some(length1), Some(length2)) = (length1, length2) {
@@ -130,14 +145,14 @@ fn dfs<'a>(
     for i in 0..remaining.len() {
         // Try adding the node to the shorter path
         let node = remaining.remove(i);
-        path1.push(node.clone());
+        path1.push(node);
         dfs(
             nodes, cache, path1, path2, remaining, best_value, best_paths,
         );
         path1.pop();
 
         // Try adding the node to the second path
-        path2.push(node.clone());
+        path2.push(node);
         dfs(
             nodes, cache, path1, path2, remaining, best_value, best_paths,
         );
@@ -148,6 +163,7 @@ fn dfs<'a>(
 }
 
 fn find_best_paths<'a>(
+    mut cache: &mut ShortestPathCache<'a>,
     nodes: &'a HashMap<String, Node>,
     mut to_visit: Vec<&'a str>,
 ) -> ((Vec<&'a str>, i64), (Vec<&'a str>, i64)) {
@@ -155,8 +171,6 @@ fn find_best_paths<'a>(
     let mut best_value = 0;
     let mut path1 = vec!["AA"];
     let mut path2 = vec!["AA"];
-
-    let mut cache = ShortestPathCache::new(nodes);
 
     dfs(
         nodes,
@@ -168,8 +182,8 @@ fn find_best_paths<'a>(
         &mut best_paths,
     );
 
-    let value1 = total_path(nodes, &mut cache, &best_paths.0).1;
-    let value2 = total_path(nodes, &mut cache, &best_paths.1).1;
+    let value1 = cache.total_path(&best_paths.0).1;
+    let value2 = cache.total_path(&best_paths.1).1;
 
     ((best_paths.0, value1), (best_paths.1, value2))
 }
@@ -190,10 +204,11 @@ pub fn calc_best_twin_path(filename: &str) -> Result<i32> {
         let mut connections = Vec::new();
 
         for i in 9..input.len() {
-            connections.push(input[i].replace(",", ""));
+            let name = input[i].replace(",", "");
+            let v = connections.push(name);
         }
         valves.insert(
-            name.clone(),
+            name,
             Node {
                 value: rate,
                 connections,
@@ -201,17 +216,23 @@ pub fn calc_best_twin_path(filename: &str) -> Result<i32> {
         );
     }
 
-    let mut to_visit = vec![
+    let mut valid_valves = vec![
         "NK", "GY", "EW", "MI", "WG", "RY", "UE", "JJ", "OA", "MC", "JE", "OQ", "QO", "GO", "JH",
     ];
 
-    let ((best_path1, best_value1), (best_path2, best_value2)) = find_best_paths(&valves, to_visit);
-    println!(
-        "Best Value: {} Best path1: {:?} Best path2: {:?}",
-        best_value1 + best_value2,
-        best_path1,
-        best_path2
-    );
+    let mut cache = ShortestPathCache::new(&valves);
+
+    for i in 1..valid_valves.len() {
+        let to_visit = valid_valves[0..i].to_vec();
+        let ((best_path1, best_value1), (best_path2, best_value2)) =
+            find_best_paths(&mut cache, &valves, to_visit);
+        println!(
+            "Best Value: {} Best path1: {:?} Best path2: {:?}",
+            best_value1 + best_value2,
+            best_path1,
+            best_path2
+        );
+    }
 
     return Ok(0);
 }
